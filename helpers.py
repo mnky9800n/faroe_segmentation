d88850485f8fedfe732675a6a03179fd884403e7
john <johnm.aiken@gmail.com>
2021-04-22 15:18:39 +0200

    moved functions into helpers module

--- helpers.py
+++ helpers.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.signal import find_peaks
 
 def remove_data_based_on_radius(data, mask_value=-1):
     """
@@ -29,3 +30,29 @@ def zscore(data):
     std = np.std(data_flat)
     
     return (data - mean)/std
+
+def get_peaks_means_stds(img, height, width):
+    """
+    finds peaks in histogram across all values in image
+    """
+    h, e = np.histogram(img[~np.isnan(img)].flatten(), bins=1000)
+    peaks, other = find_peaks(h, height=height, width=width)
+    means = e[peaks]
+    stds = np.std(img[~np.isnan(img)].flatten())/3
+    
+    return dict(zip(('hist', 'edges', 'peaks', 'means', 'stds'), (h, e, peaks, means, stds)))
+
+def label_by_peaks(img, peaks):
+    """
+    labels image pixels by nearest peak location
+    """
+    zeros = np.zeros_like(img)
+    std = peaks['stds']
+    for n, m in enumerate(peaks['means']):
+        plower = m - std
+        pupper = m + std
+        x, y = np.where(np.logical_and(img>plower, img<pupper))
+        zeros[x, y] = n + 1
+        
+#     return helpers.remove_data_based_on_radius(zeros, mask_value=np.nan)
+    return remove_data_based_on_radius(zeros, mask_value=np.nan)
